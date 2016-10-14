7a5fc5530894e30e59a8e5548acb28c4df64fce6
Caitlin Flattery <caitlinflattery@gmail.com>
2016-10-14 13:41:07 -0500

    completed baby steps

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('HELLO WORLD')
+
+//EXERCISE 1 'HELLO WORLD'
+// var hello = function() {
+//     console.log('HELLO WORLD')
+// }
+// hello()
+
+//EXERCISE 2 'BABY STEPS'
+var add = function() {
+    var cmdArr = process.argv
+    var sum = 0
+
+    for(var i = 2; i < cmdArr.length; i++) {
+        var num = parseInt(cmdArr[i])
+        sum = sum + num
+    }
+    console.log(sum)
+}
+add()
+
+// console.log(process.argv)
