8aa7e17b50c545d04bbc8576ea9f9907f98c6e07
Caitlin Flattery <caitlinflattery@gmail.com>
2016-10-18 14:34:51 -0500

    finished first i/o

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+var fs = require('fs')
 
 //EXERCISE 1 'HELLO WORLD'
-// var hello = function() {
-//     console.log('HELLO WORLD')
-// }
+var hello = function() {
+    console.log('HELLO WORLD')
+}
 // hello()
 
 //EXERCISE 2 'BABY STEPS'
 var add = function() {
+    //process.argv is a global variable
     var cmdArr = process.argv
     var sum = 0
 
+    //start at 2 because the first arg is always node and the second is always the file path
     for(var i = 2; i < cmdArr.length; i++) {
         var num = parseInt(cmdArr[i])
         sum = sum + num
     }
     console.log(sum)
 }
-add()
+// add()
 
-// console.log(process.argv)
+// EXERCISE 3
+var countNewLines = function() {
+    var testFile = process.argv[2]
+    var bufferObj = fs.readFileSync(testFile)
+    var testString = bufferObj.toString()
+    var newLines = testString.split('\n')
+
+    console.log(newLines.length - 1)
+}
+countNewLines()
